a795ee8510bed42681ead80e1804a8f9c6d71c61
Charles Wang <charleswang@Charless-MacBook-Air.local>
2019-10-26 11:04:40 -0700

    added auth keys and basic templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+import os
 from app import app
+from flask import render_template
+
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'ethan'}
+    return render_template('index.html', title='Home', user=user)
