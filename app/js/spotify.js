2ae6303d9d03aa8d9285919ebcb43c197b15d6ae
sf-18 <hdays112397@gmail.com>
2019-10-26 13:01:18 -0700

    Update spotify.js
    I need user-top-read and user-read-recently-played to get listening habits

--- app/js/spotify.js
+++ app/js/spotify.js
@@ -1,5 +1,5 @@
 app.get('/login', function(req, res) {
-var scopes = 'user-read-private user-read-email';
+var scopes = 'user-read-private user-read-email user-top-read user-read-recently-played';
 res.redirect('https://accounts.spotify.com/authorize' +
   '?response_type=code' +
   '&client_id=' + my_client_id +
