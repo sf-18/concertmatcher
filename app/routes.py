12552c92080654d06090211e062ebd001da79a8f
Charles Wang <charleswang@Charless-MacBook-Air.local>
2019-10-26 18:55:21 -0700

    finished spotify auth

--- app/routes.py
+++ app/routes.py
@@ -18,8 +18,9 @@ user_access_token, user_refresh_token = None, None
 @app.route('/')
 @app.route('/index')
 def index():
+    print(user_access_token)
     if user_access_token:
-        user = {'username': 'ethan'}
+        user = {'token': user_access_token}
         return render_template('index.html', title='Home', user=user)
     else:
         return redirect("/login")
@@ -28,28 +29,30 @@ def index():
 def login():
     return render_template('login.html', cid=client_id)
 
+@app.route('/logout')
+def logout():
+    user_access_token, user_refresh_token = None, None
+    return render_template('login.html', cid=client_id)
+
 @app.route('/callback/')
 def callback():
     url = request.url
     # Converts url into a query object and extracts auth code
     query = parse_qs(urlparse(url).query)
-    # print(query)
     if 'code' in query:
+        global user_access_token, user_refresh_token
         code = query['code']
-        # print("auth code: ", code)
-        # print("encoded: ", base64.b64encode(client_id + ':' + client_secret))
-
         # Construct post request to retrieve access token
         data = {'grant_type':'authorization_code',
                 'code': code,
-                'redirect_uri': url}
-        headers = {'Authorization': 'Basic' + base64.b64encode((client_id + ':' + client_secret).encode('utf-8'))}
-        # params = {'client_id': client_id, 'client_secret': client_secret}
-        response = requests.post('https://accounts.spotify.com/api/token', data=data, headers=headers)
-        print(response)
-        response = response.json()
+                'redirect_uri': request.base_url,
+                'client_id': client_id,
+                'client_secret': client_secret}
 
+        response = requests.post('https://accounts.spotify.com/api/token', data=data)
+        response = response.json()
         user_access_token, user_refresh_token = response["access_token"], response["refresh_token"]
+
         return redirect('/index')
     else:
         return 'Error in authorization'
