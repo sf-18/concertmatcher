831adaddead780810f34823ac9510eb46071f1fd
Ethan Lee <leeethan37@gmail.com>
2019-10-26 19:23:29 -0700

    rough draft of website

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Charles'}
+    return render_template('index.html')
