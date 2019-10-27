13571655e6d13af224e8b58c34b7a3a50859f086
Charles Wang <charleswang@Charless-MacBook-Air.local>
2019-10-26 20:23:12 -0700

    charles's changes to front-end

--- app/js/spotify.js
+++
@@ -1,8 +0,0 @@
-app.get('/login', function(req, res) {
-var scopes = 'user-read-private user-read-email user-top-read user-read-recently-played';
-res.redirect('https://accounts.spotify.com/authorize' +
-  '?response_type=code' +
-  '&client_id=' + my_client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri));
-});
