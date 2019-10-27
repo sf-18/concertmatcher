99e3b4dabbf8fa1ec478352920b198a8d92b2200
sf-18 <hdays112397@gmail.com>
2019-10-26 17:18:03 -0700

    adding in friends for a certain concerts functionality

--- backend/concertmatcher.py
+++ backend/concertmatcher.py
@@ -17,7 +17,6 @@ SEATGEEK_URL = os.environ['SEATGEEK_URL']
 SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
 SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
 SPOTIFY_URL = os.environ['SPOTIFY_API_URL']
-GENERIC_SPOTIFY_ACCESS_TOKEN = os.environ['GENERIC_SPOTIFY_ACCESS_TOKEN']
 
 REDIRECT_URI = 'http://localhost:8888' # temporary
 
@@ -49,6 +48,7 @@ association = db.Table('association',
 def concerts(user):
 	"""Returns concerts for a given user.""" 
 	artists = top_artists(user)
+	for 
 	artists = [artist.replace(" ", "+") for artist in artists]
 	query_strings = [ SEATGEEK_URL + "events?q=" + artist + "&client_id=" + SEATGEEK_CLIENT_ID for artist in artists]
 	concerts = [] 
@@ -61,17 +61,25 @@ def top_artists(user):
 	""" Returns artists that the user is interested in."""
 	query = SPOTIFY_URL + '/me/top/artists' + '?access_token=' + user.spotify_access_token
 	r = requests.get(query)
-	return r.json() 
+	return r.json()["items"]
 
-def related_artists(artist_id):
+def related_artists(artist_id, user):
 	""" Returns related artists given a certain artist_id. """
-	query = SPOTIFY_URL + '/artists/' + artist_id + '/related-artists' + '?access_token=' + GENERIC_SPOTIFY_ACCESS_TOKEN
-	return r.json()
+	query = SPOTIFY_URL + '/artists/' + artist_id + '/related-artists' + '?access_token=' + user.spotify_access_token
+	return r.json()["artists"]
 
 
 def friends(concert):
 	""" Returns the Users to go with for a given
 		concert. Concert is the json data returned by the SeatGeek API."""
+	artists = [performer["name"] for performer in concert["performers"]]
+	users = [] 
+	for artist in artists:
+		artist_obj = Artist.query.filter_by(name=artist) # each name should be unique
+		users.append(artist_obj.users) # append or concatenate - TODO: check behavior
+	return users
+
+
 	
 
 	
